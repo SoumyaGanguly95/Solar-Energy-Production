0463944e1a28892b41bbff9b2b158f968e5376f2
Soumya <soumyaganguly95@gmail.com>
2023-03-03 15:14:34 +0100

    feat: on click event added

--- geo-app.js
+++ geo-app.js
@@ -1,4 +1,5 @@
-var map = L.map("map").setView([51.96, 7.62], 11);
+var map = L.map("map").setView([52.3, 13.13], 10);
+
 var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
@@ -7,11 +8,24 @@ var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 osm.addTo(map);
 
-var layerSolarFarm = L.tileLayer.wms("http://localhost:8080/geoserver/wms", {
-  layers: "	Solar-Farm-Workspace:dataset-polygon",
+// var wms_url = "";
+
+var solarPowerLayer = L.tileLayer.wms("http://localhost:8080/geoserver/wms", {
+  layers: "Solar-Farm-Workspace:solar-farm-polygon-data",
   format: "image/png",
   transparent: true,
   attribution: "mylayer",
 });
 
-layerSolarFarm.addTo(map);
+solarPowerLayer.addTo(map);
+
+// Define a function to handle click events on the Solar Power Layer
+function onSolarPowerLayerClick(event) {
+  var shape = event.layer.feature.properties;
+  var popupContent = "<b>" + shape.Name + "</b><br>" + shape.area;
+  event.layer.bindPopup(popupContent);
+  console.log("Click");
+}
+
+// Add a click event listener to the Solar Power Layer
+solarPowerLayer.on("click", onSolarPowerLayerClick);
